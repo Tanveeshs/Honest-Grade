e6e8212d262a47cde4cbba30e91121d353b505d1
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-06-04 14:21:22 -0400

    components created - questions, answer, progree, question, and answers.  Handle click created and working.

---
+++ src/components/Progress.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function Progress(props) {
+    
+
+    return(
+
+        <h2>
+            Question {props.current} of {props.total}
+        </h2>
+    );
+}
+
+export default Progress;
