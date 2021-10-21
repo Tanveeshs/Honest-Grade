852eafe35325806f593c196fd293c721f33fbdad
Tanveesh Singh <Tanveeshs@gmail.com>
2021-10-21 17:00:56 +0530

    Test End Done

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -24,6 +24,7 @@ export function Quiz(props) {
         questions:questions1,
         assessmentId,
         examId,
+        numberQuestions1:numberQuestions,
         currentQuestion: 0,
         currentAnswer: '',
         answers: [],
@@ -32,13 +33,26 @@ export function Quiz(props) {
         end_quiz_flag:false
     }
     const [state, dispatch] = useReducer(quizReducer, initialState)
-    let {first,questions, currentQuestion, currentAnswer, answers, error, end_quiz_flag } = state;
+    let {first,questions, currentQuestion, currentAnswer, answers, error, end_quiz_flag,numberQuestions1 } = state;
+    if(end_quiz_flag){
+        return (<div>
+
+            TEST        END
+        </div>)
+    }
+
+
+
+
     let questionArray;
     if(first) {
        questionArray = questions;
     }else {
         questionArray = questions1;
     }
+
+
+
     console.log("QUESTION ARRAY",questionArray);
     const question = questionArray[currentQuestion];
     // console.log("STATE",state);
@@ -51,12 +65,33 @@ export function Quiz(props) {
         return <div className='error'>{error}</div>
     }
     const next = async () => {
+        console.log(state)
         const answer = {
             questionId: question.question._id,
             answer: currentAnswer
         }
         if (!currentAnswer) {
             dispatch({ type: SET_ERROR, error: 'Please select an option!' })
+            return;
+        }
+        if((currentQuestion+1)>=numberQuestions1){
+            console.log("IS IT HERE?")
+            await dispatch({type:'END_QUIZ', end_quiz_flag:true})
+            try{
+                await axios.post('https://honestgrade.herokuapp.com/assessment/answerQuestion',{
+                    assessmentId:assessmentId,
+                    examId:examId,
+                    questionId1:answers[0].questionId,
+                    questionId2:answer.questionId,
+                    answer1:answers[0].answer,
+                    answer2:answer.answer
+                })
+                    .then(res=>console.log(res.data))
+                    .catch(err=>console.log(err))
+            }
+            catch(err){
+                console.log('ERR',err)
+            }
             return;
         }
         await dispatch({ type: SET_ANSWERS, answers: [...answers, answer] })
